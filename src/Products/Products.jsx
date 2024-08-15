8f12946dc373bf69be8df30568bb7313aa279fcc
imnazmul <nazmulislam2322@gmail.com>
2024-08-15 19:54:22 +0600

    getting filterOptions

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 const Products = () => {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
+  const [filterOptions, setFilterOptions] = useState({})
   const [totalDataLength, setTotalDataLength] = useState(0);
   const totalPages = Math.ceil(totalDataLength / 12);
 
@@ -16,21 +17,27 @@ const Products = () => {
         setData(res.data.data);
         setTotalDataLength(res.data.totalDocCount);
       });
-
   }, [page, search]);
+
+  useEffect(()=>{
+
+  },[])
 
   return (
     <div className="max-w-screen-2xl mx-auto my-4">
       <h1 className="text-center text-xl mb-2">Products..</h1>
-      <div className="mb-4">
-        <input onChange={(e)=>setSearch(e.target.value)}
+      <div className="mb-4 flex justify-between">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
           placeholder="Search..."
           type="text"
           className="bg-gray-400 px-4 py-2 outline-none placeholder:text-gray-200 rounded"
         />
-        <select name="" id=""></select>
+        <div>
+          <select name="" id=""></select>
+        </div>
       </div>
-      
+
       <div className="p-1 md:p-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 lg:gap-3 md:gap-2 gap-1">
         {data.map((item, i) => (
           <ProductCard data={item} key={i} />
@@ -47,9 +54,11 @@ const Products = () => {
 
         {[...Array(totalPages)].map((_, i) => (
           <button
-            onClick={()=>setPage(i + 1)}
+            onClick={() => setPage(i + 1)}
             key={i}
-            className={`bg-blue-500 px-3 py-1 text-white text-xl rounded ${page === i+1 ? "bg-blue-600" : "bg-blue-500"}`}>
+            className={`bg-blue-500 px-3 py-1 text-white text-xl rounded ${
+              page === i + 1 ? "bg-blue-600" : "bg-blue-500"
+            }`}>
             {i + 1}
           </button>
         ))}
