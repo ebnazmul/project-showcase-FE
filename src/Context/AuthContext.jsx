bc58902fcc97a8ff5010b48784adf062b8e6173b
imnazmul <nazmulislam2322@gmail.com>
2024-08-16 15:56:57 +0600

    some auth update

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,12 +1,35 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
+import auth from "../config";
 
-const AuthContexts = createContext(null);
+export const AuthContexts = createContext(null);
+const googleProvaider = new GoogleAuthProvider();
 
 // eslint-disable-next-line react/prop-types
 const AuthContext = ({ children }) => {
-  const value = {
+  const [user, setUser] = useState({});
 
-    
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+
+  const continueWithGoogle = () => {
+    signInWithPopup(auth, googleProvaider).then((res) => console.log(res));
+  };
+
+  const value = {
+    user,
+    continueWithGoogle,
   };
 
   return (
