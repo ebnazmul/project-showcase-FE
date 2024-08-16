8e31bdb678f5e836126ade1ad039ecab9f95f345
imnazmul <nazmulislam2322@gmail.com>
2024-08-16 12:22:03 +0600

    onclick fn

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -2,7 +2,12 @@
 const Login = () => {
     return (
         <div>
-            
+            <form action="">
+                <div>
+                    <h4>Email:</h4>
+                    <input placeholder="Your email..." type="text" />
+                </div>
+            </form>
         </div>
     );
 };
--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,17 +1,20 @@
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../componant/Button";
 
 const Header = () => {
+  const navigate = useNavigate()
+  
   return (
     <div className="w-full bg-gray-700/20">
       <div className="max-w-screen-2xl mx-auto py-4 flex justify-between p-1 md:p-2">
         <div>
-          <img
+          <Link to="/"><img
             className="w-12 h-12"
             src="https://cdn-icons-png.flaticon.com/512/2489/2489815.png"
             alt=""
-          />
+          /></Link>
         </div>
-        <Button text="Sign In" />
+        <Button fn={()=>navigate("/login")} text="Sign In" />
       </div>
     </div>
   );
